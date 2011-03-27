9224920415396f8c33c911601da48c27241de6f1
Javier Muniz <javier@granicus.com>
2011-03-27 11:10:12 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Huzzah::Application.config.secret_token = 'a132b2178eebb1d13e7f6e5b424dc9f7ff039691a75872dfa3e44d2b0a171bbb73295eeb3f5c5a1fe3625a4e8594f9de3ab512fd0cd045b6dbd917e2ce3e0fcf'
