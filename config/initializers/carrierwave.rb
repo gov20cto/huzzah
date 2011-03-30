9f632c51b8fd72508115147c4e88863aaacd361b
Javier Muniz <javier@granicus.com>
2011-03-29 20:32:18 -0700

    trying to fix heroku mongodb config issues for carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,10 @@
 CarrierWave.configure do |config|
   config.grid_fs_database = Mongoid.database.name
   config.grid_fs_host = Mongoid.database.connection.primary_pool.host
-  #config.grid_fs_port = Mongoid.database.port
-  #config.grid_fs_username = Mongoid.database.username
-  #config.grid_fs_password = Mongoid.database.password
+  uri = URI.parse(ENV['MONGOHQ_URL'])
+  config.grid_fs_port = uri.port unless uri.port.blank?
+  config.grid_fs_username = uri.user unless uri.user.blank?
+  config.grid_fs_password = uri.password unless uri.password.blank?
   config.storage = :grid_fs
   config.grid_fs_access_url = "/images"
   config.cache_dir = "uploads"
