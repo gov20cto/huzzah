1c413e3f9d1b0c6d074fc9174df6751c3c041691
Javier Muniz <javier@granicus.com>
2011-03-29 20:27:25 -0700

    updated carrierwave config to store tmp files properly on heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,11 @@
 CarrierWave.configure do |config|
   config.grid_fs_database = Mongoid.database.name
   config.grid_fs_host = Mongoid.database.connection.primary_pool.host
+  #config.grid_fs_port = Mongoid.database.port
+  #config.grid_fs_username = Mongoid.database.username
+  #config.grid_fs_password = Mongoid.database.password
   config.storage = :grid_fs
   config.grid_fs_access_url = "/images"
+  config.cache_dir = "uploads"
+  config.root = Rails.root.join('tmp')
 end
