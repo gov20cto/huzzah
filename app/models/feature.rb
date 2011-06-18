9c9bd4eed7b886daec1161c2cdc8561d08371d74
Javier Muniz <javier@granicus.com>
2011-06-18 12:02:59 -0700

    modeled AED db

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,16 +1,56 @@
 class Feature
   include Mongoid::Document
   
-  # every geographic feature needs to have a location and an address
-  field :location, :type => Array, :geo => true, :lat => :latitude, :lng => :longitude
-  geo_index :location
+  field :internal_id, :type => Integer
+  
+  field :internal_verified_by, :type => String
+  
+  field :supervising_physician, :type => String
+  
+  field :organization, :type => String
   
   field :address, :type => String
+  
+  field :zip, :type => String
+  
+  field :exact_location, :type => String
+  
+  field :best_access, :type => String
+  
+  field :internal_last_located, :type => Date
+  
+  field :internal_follow_up, :type => String
+  
+  field :contact, :type => String
+  
+  field :contact_title, :type => String
+  
+  field :contact_phone, :type => String
+  
+  field :contact_fax, :type => String
+  
+  field :contact_email, :type => String
+  
+  field :contact_address, :type => String
+  
+  field :emergency_contact, :type => String
+  
+  field :emergency_phone, :type => Phone
+  
+  field :training_org, :type => String
+  
+  field :aed_type, :type => String
+  
+  field :number_units, :type => Integer
+  
+  field :number_users, :type => Integer
+  
+  field :date_implemented, :type => Date
 
   # each feature can have an optional image
   mount_uploader :image, FeatureImageUploader
   
-  # features are submitted by users
-  field :submitter, :type => String
-  
+  # every geographic feature needs to have a location and an address
+  field :location, :type => Array, :geo => true, :lat => :latitude, :lng => :longitude
+  geo_index :location
 end
